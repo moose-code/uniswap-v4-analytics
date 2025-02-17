8fe59bcc31757f2af90e5063abdcbdf7485ba274
moose-code <jonjonclark@gmail.com>
2025-02-17 11:41:48 +0000

    debug api keys

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-}
+  env: {
+    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
